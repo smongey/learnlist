c186436acbf29f9393cf60fae0ac0ed7e60f8b7e
Sean <sean@aokstudio.com>
2014-06-05 23:20:31 +0100

    added index limit to 20 and validated url field

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,4 +2,5 @@ class Link < ActiveRecord::Base
 	belongs_to :user
 	has_attached_file :image, :styles => { :medium => "300x", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+	validates :url, :format => URI::regexp(%w(http https))
 end
