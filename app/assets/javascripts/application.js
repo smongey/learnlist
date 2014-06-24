6b103b2e67962c715dcdbf401c051fd49a0dd375
Sean <sean@aokstudio.com>
2014-06-24 23:24:56 +0100

    fluid masonry

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,11 +16,7 @@
 //= require_tree .
 //= require masonry/jquery.masonry
 
-/*! waitForImages jQuery Plugin 2013-07-20 */
-!function(a){var b="waitForImages";a.waitForImages={hasImageProperties:["backgroundImage","listStyleImage","borderImage","borderCornerImage","cursor"]},a.expr[":"].uncached=function(b){if(!a(b).is('img[src!=""]'))return!1;var c=new Image;return c.src=b.src,!c.complete},a.fn.waitForImages=function(c,d,e){var f=0,g=0;if(a.isPlainObject(arguments[0])&&(e=arguments[0].waitForAll,d=arguments[0].each,c=arguments[0].finished),c=c||a.noop,d=d||a.noop,e=!!e,!a.isFunction(c)||!a.isFunction(d))throw new TypeError("An invalid callback was supplied.");return this.each(function(){var h=a(this),i=[],j=a.waitForImages.hasImageProperties||[],k=/url\(\s*(['"]?)(.*?)\1\s*\)/g;e?h.find("*").addBack().each(function(){var b=a(this);b.is("img:uncached")&&i.push({src:b.attr("src"),element:b[0]}),a.each(j,function(a,c){var d,e=b.css(c);if(!e)return!0;for(;d=k.exec(e);)i.push({src:d[2],element:b[0]})})}):h.find("img:uncached").each(function(){i.push({src:this.src,element:this})}),f=i.length,g=0,0===f&&c.call(h[0]),a.each(i,function(e,i){var j=new Image;a(j).on("load."+b+" error."+b,function(a){return g++,d.call(i.element,g,f,"load"==a.type),g==f?(c.call(h[0]),!1):void 0}),j.src=i.src})})}}(jQuery);
-
 function popState() {
-	$('#single, #ghost').hide();	
 	$('.link a.in').on('click', function(e){
 		e.preventDefault();
 
@@ -50,16 +46,18 @@ function popState() {
 
 
 function reReady(){
-	var $container = $('.container');
+	//var $container = $('.container');
+	
 	// initialize
-	$container.masonry({
-		itemSelector: '.link',
-		columnWidth: 300,
-		gutterWidth: 50,
-		isAnimated: true
-	});
+	// $container.masonry({
+	// 	itemSelector: '.link',
+	// 	columnWidth: '.link',
+	// 	gutterWidth: 20,
+	// 	isAnimated: true
+	// });
 
 	popState();
+	masonry();
 }
 
 
@@ -77,31 +75,32 @@ $(document).ready(function(){
 	$('.home').empty().append(randomSymbol());
 	document.title = randomSymbol();
 
+	reReady();
 
-
-
-});
-
-$(window).load( function(){
-	reReady()
 });
 
 $(document).on('page:load', function(){
 	
 	reReady();
-
-	$('.container').waitForImages(function(){
-		var $container = $(".container");
-		$container.imagesLoaded(function() {
-			return $container.masonry({
-				itemSelector: '.link',
-				columnWidth: 300,
-				gutterWidth: 50,
-				isAnimated: true
-			});
-    });
-	});
+	
 });
 
+function masonry() {
+    $('.container').waitForImages(function(){
+		var container = document.querySelector('.container');
 
+		var msnry = new Masonry( container, {
+			itemSelector: '.link',
+			columnWidth: '.link',
+			gutterWidth: 20,
+			isAnimated: true
+		});
 
+		//imagesLoaded( container, function() {
+			msnry.layout();
+		//});
+
+	});
+    // console.log('ran');
+};
+  
